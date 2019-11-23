e35334d52831bc3e21ae6c6900450f7b3f2f135d
prophecy <thearyanahmed@gmail.com>
2019-11-23 11:39:04 +0600

    Added auth_user_can helper

--- helpers/permit.php
+++ helpers/permit.php
@@ -3,8 +3,15 @@
 use Prophecy\Permit\Permit;
 
 if(!function_exists('user_can')) {
-    function user_can($permission,$module,bool $findInSession = true) {
+    function user_can(string $permission,string $module,$roleId) {
         $permit = app()->make(Permit::class);
-        return $permit->can($module,$permission,$findInSession);
+        return $permit->can($permission,$module,$findInSession);
+    }
+}
+
+if(!function_exists('auth_user_can')) {
+    function auth_user_can($permission,$module,bool $findInSession = true) {
+        $permit = app()->make(Permit::class);
+        return $permit->authUserCan($permission,$module,$findInSession);
     }
 }
