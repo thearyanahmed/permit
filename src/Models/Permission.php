fdab7f342133634f190b01f37bf209368906d615
prophecy <thearyanahmed@gmail.com>
2019-11-23 10:12:09 +0600

    Update Module -> permissions relationship.

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -4,9 +4,14 @@ namespace Prophecy\Permit\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Permission extends Model
+class Module extends Model
 {
-    protected $table = 'permissions';
+    protected $table = 'modules';
 
     protected $guarded = ['id'];
+
+    public function permissions()
+    {
+        return $this->hasManyThrough(Permission::class,Ability::class,'module_id','id','id','permission_id');
+    }
 }
