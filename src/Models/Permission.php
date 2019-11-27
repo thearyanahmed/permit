9a4e1724ffb3f99c59cda346f64360c6882bef54
prophecy <thearyanahmed@gmail.com>
2019-11-27 16:50:14 +0600

    fixed table name

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    protected $table = 'modules';
+    protected $table = 'permissions';
 
     protected $guarded = ['id'];
 
