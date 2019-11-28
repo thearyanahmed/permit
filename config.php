b400ced62b4c75c42b79e9b1b917967b25018135
prophecy <thearyanahmed@gmail.com>
2019-11-28 11:34:34 +0600

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,6 @@
 return [
     'session_role_key'      => '_user_role',
     'session_abilities_key' => '_user_abilities',
-    'redirect_to'           => 'welcome'
+    'redirect_to'           => 'welcome',
+    'find_in_session'       => false
 ];
